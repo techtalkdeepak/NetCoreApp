a1b4271847b820526171b7ac0e2dcc3e496ceee9
rashmt <rashmt@MININT-INA4DOR>
2016-07-11 13:17:51 +0530

    about page

--- src/MyFirstApp/Controllers/HomeController.cs
+++ src/MyFirstApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MyFirstApp.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+           // ViewData["Message"] = "Your application description page.";
 
             return View();
         }
--- src/MyFirstApp/Startup.cs
+++ src/MyFirstApp/Startup.cs
@@ -76,7 +76,7 @@ namespace MyFirstApp
             app.UseIdentity();
 
             // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
-
+            
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
@@ -84,5 +84,6 @@ namespace MyFirstApp
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
     }
 }
